43db0541686c20eda357848e3ad5abc8e07b69f8
Donatron <don.macarthur68@gmail.com>
2019-04-27 08:46:44 +1000

    reorganised folder structures, removed superfluous files

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,20 @@ module.exports = {
     path: path.resolve(__dirname, "assets/js"),
     publicPath: ""
   },
+  devServer: {
+    historyApiFallback: true,
+    hot: true,
+    inline: true,
+
+    host: "localhost", // Defaults to `localhost`
+    port: 8080, // Defaults to 8080
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        secure: false
+      }
+    }
+  },
   module: {
     rules: [
       {
